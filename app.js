eef48f604898dedd86b0ef48b02d84face94ce7e
preetiyadav30 <py132430@gmail.com>
2022-12-20 17:38:18 +0530

    made some canges

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const express = require("express");
-const app = express();
+
+const express=require('express');
+var https = require('https');
+var fs = require('fs');
+
+const cors=require('cors')
+const app=express();
+
+var options = {
+    key: fs.readFileSync('../ssl/privatekey.key'),
+    cert: fs.readFileSync('../ssl/certificate.pem'),   
+};
+
+app.use(express.json());
+
+let corOption ={
+    origin:['https://rslsofttech.com','https://rslsofttech.com/admin','https://admin.rslsofttech.com','https://demo.rslsofttech.com'],
+    methods:["GET","POST","PUT","DELETE"],
+  }
+  app.use(cors(corOption));
+
+  app.options('*', cors());
+
+  app.use(indexrouter)
+
 app.get("/", (req, res) => {
-    res.send(
-      `<h1 style='text-align: center'>
-            Wellcome to Nodejs 
-            <br><br>
-            <b style="font-size: 182px;">😃👻</b>
-        </h1>`
-    );
-  });
-app.listen(9000,()=>{console.log("db connected")})
+  res.send(
+    `<h1 style='text-align: center'>
+          Wellcome to my first Automatic Deployment With Github Actions
+          <br><br>
+          <b style="font-size: 182px;">😃👻</b>
+      </h1>`
+  );
+});
+
+https.createServer(options, app).listen(9000,() => console.log("App running in port 9000 !"));
