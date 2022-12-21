e375d6e8aaed5b5e42d49f5d9693785140210716
preetiyadav30 <py132430@gmail.com>
2022-12-21 12:54:16 +0530

    change app.js with new code

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-import express from 'express';
-
-import cors from 'cors';
-import fs from 'fs'
-import https from 'https'
-
-const app = express()
-
-const PORT = 9000;
-
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs")
+const https = require("https");
+const app = express();
+const port = 9000;
 var options = {
-    key: fs.readFileSync('../ssl/privatekey.key'),
-    cert: fs.readFileSync('../ssl/certificate.pem')
+  key: fs.readFileSync('../ssl/privatekey.key'),
+  cert: fs.readFileSync('../ssl/certificate.pem'),
+ 
 };
-
+app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.use(function (req, res, next) {
@@ -23,21 +20,10 @@ app.use(function (req, res, next) {
   next();
 });
 
-
-<<<<<<< HEAD
-// simple route
 app.get("/", (req, res) => {
   res.send(
     `<h1 style='text-align: center'>
-          Wellcome to Automatic Deployment With Github Actions
-=======
-  app.use(indexrouter)
-
-app.get("/", (req, res) => {
-  res.send(
-    `<h1 style='text-align: center'>
-          Wellcome to my first Automatic Deployment With Github Actions and aapanel
->>>>>>> 77ef77c64b466752cc32adab438aec988cab3fe0
+          Wellcome to RSL Backend 
           <br><br>
           <b style="font-size: 182px;">😃👻</b>
       </h1>`
@@ -45,4 +31,3 @@ app.get("/", (req, res) => {
 });
 
 https.createServer(options, app).listen(9000,() => console.log("App running in port 9000 !"));
-
